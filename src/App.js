d8fdd59f04affff965b189095f960e25a5146948
davidwale <davidwale2003@gmail.com>
2024-05-21 21:18:17 +0100

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <div className='App'>
-     <Home />
+      <Home />
+      <Analytics />
     </div>
   );
 }
